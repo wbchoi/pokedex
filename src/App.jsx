c7fa626e2c97a42e260b8e5e1ecfee05716f3e7b
Matheus Bai <baiyj.matheus@gmail.com>
2025-03-24 22:55:43 -0300

    chore: add pokemon response (#1)
    Co-authored-by: Matheus Bai <bai.matheus@quintoandar.com.br>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import { useState } from 'react'
-
 function App() {
+
+  // mock data
+  const pokemons = [
+    {
+      "id": 1,
+      "name": "bulbasaur",
+      "height": 7,
+      "weight": 69,
+      "sprites": {
+        "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+      },
+      "types": [
+        { "type": { "name": "grass" } },
+        { "type": { "name": "poison" } }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "ivysaur",
+      "height": 10,
+      "weight": 130,
+      "sprites": {
+        "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
+      },
+      "types": [
+        { "type": { "name": "grass" } },
+        { "type": { "name": "poison" } }
+      ]
+    }
+  ];
 
   return (
     <>
+      <PokemonCard pokemon={pokemons[0]} />
     </>
   )
 }
